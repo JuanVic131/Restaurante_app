78d074deb65d151bc242bef0b09e01478e331f6b
JV <preubecitas@gmail.com>
2021-08-18 14:28:44 -0600

    Se agrega el splash screen

---
+++ app/src/main/java/com/example/restauranteapp/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.restauranteapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startTimer()
+    }
+
+    fun startTimer() {
+        object: CountDownTimer(2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                startActivity(Intent(applicationContext, MainActivity::class.java).apply {  })
+            }
+        }.start()
+    }
+}
