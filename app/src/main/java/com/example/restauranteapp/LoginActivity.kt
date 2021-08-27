22d3a2923dc5d5584dd07eb355513ce922c928c0
JV <preubecitas@gmail.com>
2021-08-26 18:34:25 -0600

    Creación de usuario

--- app/src/main/java/com/example/restauranteapp/LoginActivity.kt
+++ app/src/main/java/com/example/restauranteapp/LoginActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 
@@ -39,6 +38,6 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     // Viajar al activity para registrar a usuario
     private fun viewRegistrar() {
-        startActivity(Intent(this, RegistrarUsuarioActivity::class.java).apply {  })
+        startActivity(Intent(this, CreateUserActivity::class.java).apply {  })
     }
 }
